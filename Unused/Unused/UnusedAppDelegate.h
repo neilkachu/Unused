8dace48660df480a6e0919ef65bdaa03ee4bdd94
Neil Edwards <neil_edwards@me.com>
2013-01-29 18:33:11 +0000

    Added .plist search option

--- Unused/Unused/UnusedAppDelegate.h
+++ Unused/Unused/UnusedAppDelegate.h
@@ -29,6 +29,7 @@
 @property (assign) IBOutlet NSButton *xibCheckbox;
 @property (assign) IBOutlet NSButton *cppCheckbox;
 @property (assign) IBOutlet NSButton *htmlCheckbox;
+@property (assign) IBOutlet NSButton *plistCheckbox;
 @property (assign) IBOutlet NSButton *mmCheckbox;
 @property(assign) IBOutlet NSButton *browseButton;
 @property(assign) IBOutlet NSTextField *pathTextField;
